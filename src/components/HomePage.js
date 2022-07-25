88fc9ebfbad929c6c8b8b910c13edcd9dc91fcff
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-07-25 18:19:27 +0300

    done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 import About from "./About";
 import ArticleList from "./ArticleList";
 
 function HomePage() {
-  // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
+  
+  const homePageTitle = 'Underreacted | Home'
+  const {data:posts, isLoaded} = useQuery("http://localhost:4000/posts"); // to rename data as posts
 
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
 
-  // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+  useDocumentTitle(homePageTitle)
 
   return (
     <>
--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -3,15 +3,14 @@ import { useEffect, useState } from "react";
 // take in the url
 function useQuery(url) {
   const [isLoaded, setIsLoaded] = useState(false);
-  // rename `posts` to a more generic `data`
   const [data, setData] = useState(null);
 
   useEffect(() => {
     setIsLoaded(false);
     fetch(url)
-      .then((r) => r.json())
-      .then((data) => {
-        setData(data);
+      .then((res) => res.json())
+      .then((fetchedData) => {
+        setData(fetchedData);
         setIsLoaded(true);
       });
   }, [url]);
